53b7bc0341463e2fb9eb4767f563da3bc14845b0
Jose Longhi <jose.longhi@crengland.com>
2019-11-06 14:55:43 -0600

    Initial sample code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # grpc
 Playing with .net core 3.0 and gRPC
+
+[Tutorial](https://docs.microsoft.com/en-us/aspnet/core/tutorials/grpc/grpc-start?view=aspnetcore-3.0&tabs=visual-studio-code)
---
+++ src/GrpcGreeterClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using GrpcGreeter;
+using Grpc.Net.Client;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            // The port number(5001) must match the port of the gRPC server.
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayMyNameAsync(new HelloRequest { Name = "Jose", Middlename="P.", Lastname="Longhi" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+    }
+}
